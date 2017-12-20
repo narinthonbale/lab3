84ed6fe5bbaf7bfeb8e8eb4560653e8d272e773d
narinthonbale <1997balebale@gmail.com>
2017-12-20 14:21:24 +0700

    bale

---
+++ src/main/java/rmuti/lab003/TestArrayList.java
@@ -0,0 +1,21 @@
+package rmuti.lab003;
+
+import rmuti.lab003.ArrayList;
+
+public class TestArrayList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList lst = new ArrayList();
+		lst.add(0, "10");
+		lst.add(1, "11");
+		lst.add(2, "13");
+		lst.add(3, "15");
+		lst.add(4, "20");
+		lst.add(5, "60");
+		lst.add(6, "80");
+		System.out.println(lst);
+
+	}
+
+}
